035e9d2739a711957b8ee0f9a9430627ee1b5b9f
Sartaj Singh Gill <sartaj@atomicsquare.com>
2021-01-07 01:17:15 -0500

    exercise app working

---
+++ src/components/Exercise.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="card m-3" style="width:30rem">
+        <div class="card-body">
+            <h5 class="card-title">
+                Sartaj's Daily exercise routine
+            </h5>
+        </div>
+    </div>
+    <div class="container-fluid" id="exercise">
+        <ul class="list-group">
+            <li class="list-group-item">
+                <h4>Pushups: {{ pushups }}</h4>
+                <button class="btn btn-primary" @click="addPushups"><i class="far fa-plus-circle"></i> Add</button>
+            </li>
+            <li class="list-group-item">
+                <h4>Situps: {{ situps }}</h4>
+                <button class="btn btn-primary" @click="addSitups"><i class="far fa-plus-circle"></i> Add</button>
+            </li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Exercise',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      pushups: 0,
+      situps: 0
+    }
+  },
+  methods: {
+    addPushups(){
+      this.pushups++
+    },
+    addSitups(){
+      this.situps++
+    }
+  }
+}
+</script>
