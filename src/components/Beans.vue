d92ab3a5f906fc819f60452e4601bc9fefc06dec
Sartaj Singh Gill <sartaj@atomicsquare.com>
2021-01-08 01:11:26 -0500

    Added bootstrap js file for collapsing add counter. Added collapse functionality to add counter section

--- src/components/Beans.vue
+++ src/components/Beans.vue
@@ -3,9 +3,10 @@
     <div class="card m-3 border-light">
       <h5 class="card-header text-center ">
         Bean Counter (Count anything you like - pushups completed, seconds breath held etc.)
+        <button type="button" class="btn btn-outline-dark far fa-minus float-end" data-bs-toggle="collapse" data-bs-target=".multi-collapse"></button>
       </h5>
-      <div class="card-body">
-          <div class="input-group mb-3 card-text" style="width:50%">
+      <div class="collapse multi-collapse show card-body float-start">
+          <div class="input-group mb-3 card-text container-sm">
               <span class="input-group-text">New Counter</span>
               <input type="text" name="addCounter" id="addCounter" class="form-control" v-model="addCounter" autofocus>
               <button class="input-group-text far fa-plus btn btn-primary" @click="addNewCounter"></button>
@@ -13,7 +14,7 @@
           <!--<button class="btn btn-success far fa-save mx-2" @click="storeToLocalStorage"> Save All</button>-->
           <button class="btn btn-danger far fa-times mx-2" @click="clear"> Clear All</button>
       </div>
-      <div class="card-footer text-muted text-center">
+      <div class="collapse multi-collapse show card-footer text-muted text-center">
         Built by Sartaj using VueJS
       </div>
     </div>
@@ -24,7 +25,7 @@
             {{ item.text }}
             <button class="btn btn-danger far fa-times float-end" @click="removeCounter(index)"></button>
           </h5>
-          <div class="card-body">
+          <div class="card-body disable-dbl-tap-zoom">
             {{ item.count }}
             <br />
             <button class="btn btn-outline-primary my-2 far fa-plus" @click="add(index)"></button>
